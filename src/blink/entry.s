d9a5650513a6fbaa23288fae46baae803a37a370
Лев Светлаков <l.svetlakov@iva.ru>
2024-12-28 23:21:55 +0300

    feat/add m328Pdef.inc for usage in assembler

--- src/blink/entry.s
+++ src/blink/entry.s
@@ -1,15 +1,18 @@
+.include "m328Pdef.inc"
+
 .text
 
 .globl main
+.globl _start
 _start:
     
     # allocate 4096 bytes for stack
     nop
 
     ldi r16, 0x0
-    out spl, r16
+    out SPL, r16
     ldi r16, 0x10
     out sph, r16
     
-    jmp _start
+    jmp main
     nop
